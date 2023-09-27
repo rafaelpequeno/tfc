5d5454272fe925dea21e536257e393173e80ccba
Rafael <rflpeq.rs@gmail.com>
2023-09-27 09:40:10 -0300

    Requisito 1 concluído

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,36 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import sequelize from '.';
+
+class SequelizeTeam extends Model<
+InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: 'teams',
+  tableName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default SequelizeTeam;
