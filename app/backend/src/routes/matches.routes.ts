9c826e0dd576f0ce6961fb3e1e03fbf3360d0f58
Rafael <rflpeq.rs@gmail.com>
2023-10-05 06:09:25 -0300

    Adiciona autenticação na rota que finaliza partida

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,5 +1,6 @@
 import { Request, Router, Response } from 'express';
 import MatchController from '../controller/MatchControler';
+import verifyToken from '../middlewares/auth.middleware';
 
 const matchController = new MatchController();
 
@@ -8,7 +9,7 @@ const router = Router();
 router.get('/', (req: Request, res: Response) =>
   matchController.getAll(req, res));
 
-router.patch('/:id/finish', (req: Request, res: Response) =>
+router.patch('/:id/finish', verifyToken, (req: Request, res: Response) =>
   matchController.finishMatch(req, res));
 
 export default router;
